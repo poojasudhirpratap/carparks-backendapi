667eb249a58aa5ec8d5d529656992a4031363676
Sudhir Kumar Singh <sudhirpratap87@gmail.com>
2025-12-10 14:54:19 +0530

    added a dummy api end point

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use('/api/bookings', bookingsRouter);
 
 // Health check
 app.get('/', (req, res) => res.json({ ok: true, message: 'Car Parking Management API' }));
+app.get('/sudhir', (req, res) => res.json({ ok: true, message: 'This is Sudhir Kumar Singh.' }));
 
 // Connect to MongoDB and start server
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
